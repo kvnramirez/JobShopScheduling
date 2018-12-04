d62fc90491b7a7f72b34535b2a7ab9b897fc5a33
kevin ramirez <kvnzavalza@hotmail.com>
2018-12-03 22:53:39 -0600

    Fuerza bruta

--- BruteForce.py
+++ BruteForce.py
@@ -33,24 +33,22 @@ def main():
     start = time.time()
     print(">>> Job Shop Scheduling con Fuerza Bruta <<<")
 
+    # Configuracion
     set = m10
     m_size = len(set)
+    debug = False
 
+    # Imprimir tabla de trabajos y maquinas
     table = BeautifulTable()
-
     headers = []
     for x in range(len(set)):
         headers.append(str(x))
-
     table.column_headers = headers
-
     for row in set:
         table.append_row(row)
-
     print(table)
 
-    debug = False
-
+    # Calcular combinaciones
     n = len(set)
     index_list = range(n)
     perms = itertools.permutations(index_list)
@@ -108,84 +106,6 @@ def main():
 
     end = time.time()
     print("Tiempo de ejecucion del programa: %d ms " % ((end - start) * 1000))
-
-
-# print("--M-- | T1 | T2 | T3 | T4")
-# print(" -A- ", A)
-# print(" -B- ", B)
-# print(" -C- ", C)
-# print(" -D- ", D)
-
-# perms = list(itertools.permutations(input))
-# num_perms = len(list(itertools.permutations(input)))
-# print("permutations: ", perms)
-# print("permutations size: ", num_perms)
-# sum_perms = []
-#
-# for x in perms:
-#     sum_perms.append(sum(x))
-#
-# print("sum perms: ", sum_perms)
-
-# product(A, B) returns the same as:  ((x,y) for x in A for y in B).
-# combinaciones = list(
-#     itertools.product(item.get_list_by_worker("A"), item.get_list_by_worker("B"), item.get_list_by_worker("C"),
-#                       item.get_list_by_worker("D")))
-# num_combinaciones = len(combinaciones)
-# print("Combinaciones: %s" % combinaciones)
-# print("Total combinaciones: %d" % num_combinaciones)
-#
-# # for x in combinaciones:
-# #     for y in x:
-# #         t = [i for i in t if i.name != "Remove me"]
-#
-# sum_combinacines = []
-# for x in combinaciones:
-#     sum = 0
-#     print("combinacion: ", x)
-#     for y in x:
-#         sum = y.val + sum
-#     sum_combinacines.append(sum)
-#     print("sum: ", sum)
-#     print("------")
-# print("Suma de combinaciones posibles: ", sum_combinacines)
-#
-# # index = 0
-# # elem = 0
-# # i = 0
-# #
-# # while i < len(sum_combinacines) - 1:
-# #     print("i: ", i)
-# #     if (sum_combinacines[i] < sum_combinacines[i + 1]):
-# #         print("elem: ", elem)
-# #         elem = sum_combinacines[i]
-# #         index = i
-# #     else:
-# #         elem = sum_combinacines[i + 1]
-# #         index = i + 1
-# #     i = i + 1
-#
-# print("index: ", sum_combinacines.index(min(sum_combinacines)))
-# print("Costo minimo: ", min(sum_combinacines))
-# combinacion_minima = combinaciones[sum_combinacines.index(min(sum_combinacines))]
-# print("Combinacion: ", combinacion_minima)
-#
-# print("tddd: ", B.index(3))
-#
-# # TODO cuando hace combinacion, no se puede repetir la misma columna ya que dos maquinas podrian hacer el mismo trabajo
-# # https://stackoverflow.com/questions/32726673/combinations-with-entries-unique-in-row-and-column
-# print("Maquina %d - Trabajo %d (Duracion: %d)" % (1, A.index(combinacion_minima[0]) + 1, combinacion_minima[0]))
-# print("Maquina %d - Trabajo %d (Duracion: %d)" % (2, B.index(combinacion_minima[1]) + 1, combinacion_minima[1]))
-# print("Maquina %d - Trabajo %d (Duracion: %d)" % (3, C.index(combinacion_minima[2]) + 1, combinacion_minima[2]))
-# print("Maquina %d - Trabajo %d (Duracion: %d)" % (4, D.index(combinacion_minima[3]) + 1, combinacion_minima[3]))
-#
-# # count = 1;
-# # for a in combinacion_minima:
-# #     print("Maquina %d - Trabajo %d (Duracion: %d)" % (count, combinacion_minima.index(a) + 1, a))
-# #     count = count + 1;
-# # #
-# # for y in sum_combinacines:
-#     print("hola")
 
 
 if __name__ == "__main__":
