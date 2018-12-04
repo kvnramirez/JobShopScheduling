0163c8733bfd8b8fd67f491b430b96350e6a4a33
kevin ramirez <kvnzavalza@hotmail.com>
2018-12-04 07:33:19 -0600

    Fuerza bruta

---
+++ Greedy.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# https://gist.github.com/andylolz/8312373
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ HungarianAlgorithm.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# https://pypi.org/project/munkres/
+
+# https://github.com/tdedecko/hungarian-algorithm/blob/master/hungarian.py
+
+# https://hackernoon.com/the-assignment-problem-calculating-the-minimum-matrix-sum-python-1bba7d15252d
+
+# https://www.geeksforgeeks.org/hungarian-algorithm-assignment-problem-set-1-introduction/
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
