42acad8a198576ca9422e778cd8f6d2564c70736
Kevin Ramirez <quevon24@gmail.com>
2018-12-11 11:36:51 -0600

    johnson calendariza correctamente para 3x3

--- Johnsons.py
+++ Johnsons.py
@@ -54,43 +54,43 @@ def main():
         for i in m_range:
             temp_matrix.append(copy.deepcopy(get_jobs_by_machine(i, list_jobs)))
 
-        print("temp matrix: ", temp_matrix)
+        # print("temp matrix: ", temp_matrix)
 
         left = []
         right = []
-        print("range permutation: ", range_permutation)
+        # print("range permutation: ", range_permutation)
         counter = 0
         for i in range_permutation:
-            print("machine for min job: ", i)
-            print("new temp matrix: ", temp_matrix)
-            print("new temp matrix count: ", len(temp_matrix))
+            # print("machine for min job: ", i)
+            # print("new temp matrix: ", temp_matrix)
+            # print("new temp matrix count: ", len(temp_matrix))
             min_job, min_job_index = get_min_job(temp_matrix[i])
             if (counter % 2) == 0:
-                print("{0} is Even, right set".format(i))
-                print("xx: ", temp_matrix[i])
+                # print("{0} is Even, right set".format(i))
+                # print("xx: ", temp_matrix[i])
                 left.append(min_job.id)
-                print("updated left: ", left)
+                # print("updated left: ", left)
             else:
-                print("{0} is Odd, left set".format(i))
-                print("yy: ", temp_matrix[i])
+                # print("{0} is Odd, left set".format(i))
+                # print("yy: ", temp_matrix[i])
                 right.append(min_job.id)
-                print("updated right: ", right)
+                # print("updated right: ", right)
             counter = counter + 1
             for ii in range_permutation:
-                print("ii: ", ii)
+                # print("ii: ", ii)
                 del temp_matrix[ii][min_job_index]
 
-            print("min_job_index: ", min_job_index)
-            print("-------------")
+            # print("min_job_index: ", min_job_index)
+            # print("-------------")
 
         # Ordenar de manera descendente, se reduce a una sola solucion
         # left.sort(reverse=True)
         # right.sort(reverse=True)
 
-        print("left: ", left)
-        print("right: ", right)
-
-        print("secuencia: ", left + right)
+        # print("left: ", left)
+        # print("right: ", right)
+        #
+        # print("secuencia: ", left + right)
         job_sequence = left + right
 
         for machine in m_range:
@@ -98,7 +98,7 @@ def main():
             for job in job_sequence:
                 temp_job = get_job_by_machine_and_job_id(list_jobs, job, machine)
                 new_machine.jobs.append(copy.deepcopy(temp_job))
-            print("new_machine: ", new_machine)
+            # print("new_machine: ", new_machine)
             new_solution.machines.append(new_machine)
 
         # Establecer tiempos de cada trabajo de primer maquina
@@ -111,10 +111,10 @@ def main():
                 for job in machine.jobs:
                     job[0].start_time = start
                     job[0].end_time = job[0].start_time + job[0].duration
-                    print("mx job: ", job[0])
+                    # print("mx job: ", job[0])
                     start = job[0].end_time
             x_counter = x_counter + 1
-            print("mx: ", machine)
+            # print("mx: ", machine)
 
         # Establecer tiempos de cada trabajo para las demas maquinas
         c_counter = 1
@@ -136,15 +136,15 @@ def main():
                     j_counter = j_counter + 1
                     s = c2[i][0].end_time
                 else:
-                    print("-------------")
-                    print("empezar job que no es el primero en : ", s)
-                    print("c1[i][0].id : ", c1[i][0].id)
-                    print("c1[i][0].start_time : ", c1[i][0].start_time)
-                    print("c1[i][0].end_time : ", c1[i][0].end_time)
-                    print("c2[i][0].id : ", c2[i][0].id)
-                    print("c2[i][0].start_time : ", c2[i][0].start_time)
-                    print("c2[i][0].end_time : ", c2[i][0].end_time)
-                    print("c1[i][0].end_time - s: ", c1[i][0].end_time - s)
+                    # print("-------------")
+                    # print("empezar job que no es el primero en : ", s)
+                    # print("c1[i][0].id : ", c1[i][0].id)
+                    # print("c1[i][0].start_time : ", c1[i][0].start_time)
+                    # print("c1[i][0].end_time : ", c1[i][0].end_time)
+                    # print("c2[i][0].id : ", c2[i][0].id)
+                    # print("c2[i][0].start_time : ", c2[i][0].start_time)
+                    # print("c2[i][0].end_time : ", c2[i][0].end_time)
+                    # print("c1[i][0].end_time - s: ", c1[i][0].end_time - s)
                     if c1[i][0].end_time - s > 0:
                         c2[i][0].start_time = c1[i][0].end_time
                         c2[i][0].end_time = c2[i][0].start_time + c2[i][0].duration
@@ -157,9 +157,9 @@ def main():
                         j_counter = j_counter + 1
                         s = c2[i][0].end_time
                         pass
-                    print("-----------------")
-                print("c1: ", c1[i][0])
-                print("c2: ", c2[i][0])
+                #     print("-----------------")
+                # print("c1: ", c1[i][0])
+                # print("c2: ", c2[i][0])
 
         # Agregar solucion a la lista
         solutions.append(new_solution)
@@ -173,17 +173,17 @@ def main():
     for solution in solutions:
         durations = []
         for machine in solution.machines:
-            print("machine.total_duration: ", machine.total_duration)
+            # print("machine.total_duration: ", machine.total_duration)
 
             for jobs_sequence in machine.jobs:
                 end_times = []
                 for job in jobs_sequence:
                     end_times.append(job.end_time)
-                print("Maquina %s, %s" % (machine.id, end_times))
+                # print("Maquina %s, %s" % (machine.id, end_times))
                 machine.total_duration = max(end_times)
-                print("machine.total_duration: ", machine.total_duration)
+                # print("machine.total_duration: ", machine.total_duration)
             durations.append(machine.total_duration)
-        print("durations: ", durations)
+        # print("durations: ", durations)
         solution.makespan = max(durations)
 
     print("~~~~~~~~~~~~~~~~~~~~~~~~")
