d2075f5a8d33f1f2b8edfb32b586c6326fe003bf
kevin ramirez <kvnzavalza@hotmail.com>
2018-12-11 01:46:37 -0600

    johnson calendariza correctamente para 3x3

--- Johnsons.py
+++ Johnsons.py
@@ -29,7 +29,7 @@ def main():
     print("Job Shop Scheduling con Algoritmo de Johnson")
 
     # i2, i3, i1
-    input_matrix = i2
+    input_matrix = i3
     machines_number = len(input_matrix)
     jobs_number = len(input_matrix[0])
 
